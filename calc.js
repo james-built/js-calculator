88455f30029d8e4fb5a0a6dd0e5b32ef432bad91
james-built <jgbuilt@gmail.com>
2018-07-23 00:43:54 +1200

    added conditionals to carry out appropriate action for input pressed, added calculation loop

--- calc.js
+++ calc.js
@@ -13,5 +13,47 @@ console.log("hello world");
 console.log(buttons[0].innerTEXT);
 
 function calculator(){
-  var value = 
+  var value = //keypress;
+  if (value !=== NaN || value === "."){
+    temp += value;
+    document.getElementById("display").innerHTML = temp;
+  } else if (val === "AC"){
+    temp = '';
+    inputs = [];
+    total = 0;
+    document.getElementById("display").innerHTML = '';
+  } else if (val === "CE"){
+    temp = '';
+    document.getElementById("display").innerHTML = '';
+  } else if (val === "X"){
+    inputs.push(temp);
+    inputs.push('*');
+    temp = '';
+  } else if (val === "/"){
+    inputs.push(temp);
+    inputs.push("/");
+    temp = '';
+  //calculation step if = input pressed
+  } else if (val === "="){
+    inputs.push(temp);
+    //store initial number for calculation
+    var nt = Number(entries[0]);
+    for (let i = 1; i < entries.length; i++){
+      var operand = entries[i];
+      var nextNum = Number(entries[i+1]);
+      if (operand === "*") {
+        nt *= nextNum;
+      } else if (operand === "/"){
+        nt /= nextNum;
+      } else if (operand === "+"){
+        nt += nextNum;
+      } else if (operand === "-"){
+        nt -= nextNum;
+      }
+      i++
+    }
+    if (nt < 0) {
+      
+    }
+  }
 }
