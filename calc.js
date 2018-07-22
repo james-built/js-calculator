2dc145eefd1a9a37db2567b3f0adf43be12da16e
james-built <jgbuilt@gmail.com>
2018-07-22 21:09:21 +1200

    event listener scripting not working correctly

--- calc.js
+++ calc.js
@@ -2,6 +2,16 @@ var inputs=[];
 var total=0;
 
 var temp= '';
-document.getElementsByTagName('button').addEventListener('click', function Calculator(){
+var buttons = document.getElementsByTagName("button");
+console.log(buttons);
+/*for (let i = 0; i < buttons.length; i++){
+buttons[i].addEventListener("click", function (){
 console.log("hello world");
 })
+}
+*/
+console.log(buttons[0].innerTEXT);
+
+function calculator(){
+  var value = 
+}
